f2cf8292e4fb5ab1cc16f9a3c0029617eb698625
John Galindo <john_galindo@hotmail.com>
2023-12-12 11:30:12 -0600

    Completed

---
+++ candidate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vision_API
+{
+    internal class candidate
+    {
+        public string personid;
+        public double confidence;
+        public string name;
+    }
+}
